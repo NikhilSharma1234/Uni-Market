7f746478f0723ea826cdbe10308ba52cc0cf4f8d
jacobghunter <98200224+jacobghunter@users.noreply.github.com>
2023-10-18 16:16:40 -0700

    nicer looking navbar
    added an image and made navbar only span part of the screen, messed around with making a dark theme

--- lib/navbar.dart
+++ lib/navbar.dart
@@ -9,22 +9,53 @@ class NavBar extends AppBar {
 class _NavBarState extends State<NavBar> with TickerProviderStateMixin {
   @override
   Widget build(BuildContext context) {
+    var brightness = MediaQuery.of(context).platformBrightness;
+    bool darkModeOn = brightness == Brightness.dark;
+
+    double view_width = MediaQuery.of(context).size.width;
+    double tab_size = view_width * 0.2;
     return AppBar(
-      title: const Text('TabBar Widget'),
-      bottom: TabBar(
-        controller: TabController(length: 3, vsync: this),
-        tabs: const <Widget>[
-          Tab(
-            icon: Icon(Icons.cloud_outlined),
-          ),
-          Tab(
-            icon: Icon(Icons.beach_access_sharp),
-          ),
-          Tab(
-            icon: Icon(Icons.brightness_5_sharp),
-          ),
-        ],
-      ),
+      // backgroundColor: Colors.deepPurple,
+      // leading is the image, displayed before the title and tabbar
+      leadingWidth: 150,
+      // not entirely sure why 80 is the right number to center the image, but here we are
+      toolbarHeight: 80,
+
+      leading: Align(
+          alignment: Alignment.centerLeft,
+          child: Padding(
+              padding: const EdgeInsets.all(8),
+              // switches image based on dark mode
+              child: Image.asset((darkModeOn)
+                  ? "lib/images/logo_dark.png"
+                  : "lib/images/logo_light.png"))),
+      // title: const Text('Uni-Market'),
+      flexibleSpace: Align(
+          alignment: Alignment.topRight,
+          child: TabBar(
+            isScrollable: true,
+            controller: TabController(length: 3, vsync: this),
+            tabs: <Widget>[
+              SizedBox(
+                  width: tab_size,
+                  child: const Tab(
+                    text: 'Home',
+                    icon: Icon(Icons.home),
+                  )),
+              SizedBox(
+                  width: tab_size,
+                  child: const Tab(
+                    text: 'Search',
+                    icon: Icon(Icons.search),
+                  )),
+              SizedBox(
+                  width: tab_size,
+                  child: const Tab(
+                    text: 'Login',
+                    icon: Icon(Icons.login),
+                  )),
+            ],
+          )),
     );
   }
 }
