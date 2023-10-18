eac562a1d6e7df21ec9162f0c0544d671e1b014c
jacobghunter <98200224+jacobghunter@users.noreply.github.com>
2023-10-18 14:38:59 -0700

    appbar in separate file
    temp navbar (which is an appbar) in separate file working in main screen

--- lib/navbar.dart
+++ lib/navbar.dart
@@ -2,8 +2,6 @@ import 'package:flutter/material.dart';
 
 // if you make this extend and return an AppBar widget, you can use it as the appBar: in other widgets
 class NavBar extends AppBar {
-  NavBar({super.key});
-
   @override
   State<NavBar> createState() => _NavBarState();
 }
@@ -11,60 +9,22 @@ class NavBar extends AppBar {
 class _NavBarState extends State<NavBar> with TickerProviderStateMixin {
   @override
   Widget build(BuildContext context) {
-    var brightness = MediaQuery.of(context).platformBrightness;
-    bool darkModeOn = brightness == Brightness.dark;
     return AppBar(
-      actions: <Widget>[
-        TextButton(
-          style: TextButton.styleFrom(
-              foregroundColor: Colors.white,
-              padding: const EdgeInsets.all(16.0),
-              textStyle: const TextStyle(fontSize: 20)),
-          onPressed: () {
-            // Get the current route
-            var currentRoute = ModalRoute.of(context)?.settings.name;
-
-            // Check if the current route is not the one you are trying to navigate to
-            if (currentRoute != '/signUp') {
-              Navigator.pushReplacementNamed(context, '/signUp');
-            }
-          },
-          child: const Text('Sign Up'),
-        ),
-        const SizedBox(height: 20),
-        ClipRRect(
-          borderRadius: BorderRadius.circular(4),
-          child: Container(
-            color: Colors.white,
-            child: TextButton(
-              style: TextButton.styleFrom(
-                foregroundColor: const Color(0xFF041E42),
-                padding: const EdgeInsets.all(16.0),
-                textStyle: const TextStyle(fontSize: 20),
-              ),
-              onPressed: () {
-                // Get the current route
-                var currentRoute = ModalRoute.of(context)?.settings.name;
-
-                // Check if the current route is not the one you are trying to navigate to
-                if (currentRoute != '/signIn') {
-                  Navigator.pushReplacementNamed(context, '/signIn');
-                }
-              },
-              child: const Text('Sign In'),
-            ),
+      title: const Text('TabBar Widget'),
+      bottom: TabBar(
+        controller: TabController(length: 3, vsync: this),
+        tabs: const <Widget>[
+          Tab(
+            icon: Icon(Icons.cloud_outlined),
           ),
-        ),
-      ],
-      backgroundColor: Theme.of(context).scaffoldBackgroundColor,
-      title: GestureDetector(
-          onTap: () {
-            Navigator.pushReplacementNamed(context, '/home');
-          },
-          child: Image.asset(
-              (darkModeOn) ? 'assets/logo_dark.png' : 'assets/logo_light.png',
-              fit: BoxFit.contain,
-              height: 48)),
+          Tab(
+            icon: Icon(Icons.beach_access_sharp),
+          ),
+          Tab(
+            icon: Icon(Icons.brightness_5_sharp),
+          ),
+        ],
+      ),
     );
   }
 }
