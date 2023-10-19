9391ea506e67a52795d856201b236030edc0a3ae
Nikhil <nikhil342015@gmail.com>
2023-10-19 13:35:35 -0700

    Sign Up Stepper Template

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 import 'navbar.dart';
-
-// NOTE - to get assets like images to work, you need to run flutter build web then use the asset in code
+import 'register.dart';
+import 'verify.dart';
+import 'aboutyou.dart';
 
 void main() {
   runApp(const MyApp());
@@ -48,17 +49,18 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  int index = 0;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  tapped(int step) {
+    setState(() => index = step);
+  }
+
+  continued() {
+    index < 2 ? setState(() => index += 1) : null;
+  }
+
+  cancel() {
+    index > 0 ? setState(() => index -= 1) : null;
   }
 
   @override
@@ -77,40 +79,55 @@ class _MyHomePageState extends State<MyHomePage> {
           // width not working
           preferredSize: Size(screenWidth * 0.25, 110),
           child: NavBar()),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+      body: Theme(
+        data: ThemeData(
+            hoverColor: Colors.transparent,
+            shadowColor: Colors.transparent,
+            canvasColor: const Color(0xFF041E42),
+            colorScheme: const ColorScheme.dark(
+                primary: Colors.white, secondary: Colors.white)),
+        child: Center(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              const Expanded(flex: 3, child: Column()),
+              Expanded(
+                  flex: 4,
+                  child: Column(children: <Widget>[
+                    const Padding(
+                      padding: EdgeInsets.all(10.0),
+                      child: Text(
+                        'Welcome to the marketplace made for students.',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 48,
+                          fontWeight: FontWeight.w500,
+                        ),
+                        textAlign: TextAlign.center,
+                      ),
+                    ),
+                    ConstrainedBox(
+                        constraints:
+                            const BoxConstraints.tightFor(height: 200.0),
+                        child: Stepper(
+                          type: StepperType.horizontal,
+                          currentStep: index,
+                          onStepCancel: cancel,
+                          onStepContinue: continued,
+                          onStepTapped: (step) => tapped(step),
+                          physics: const ScrollPhysics(),
+                          steps: <Step>[
+                            Register(index),
+                            Verify(index),
+                            AboutYou(index),
+                          ],
+                        ))
+                  ])),
+              const Expanded(flex: 3, child: Column())
+            ],
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
