4742efd3ea22a35dce0b88903aabcbeb522bde40
Nikhil <nikhil342015@gmail.com>
2023-10-19 13:36:11 -0700

    App Bar Changes

--- lib/main.dart
+++ lib/main.dart
@@ -65,13 +65,6 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    // return NavBar();
     double screenWidth = MediaQuery.of(context).size.width;
     return Scaffold(
       // sets height of appbar
