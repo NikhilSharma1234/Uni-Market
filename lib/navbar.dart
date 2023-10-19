c17a7ed670d5e489a6788daa81fd07c30d8f4183
Nikhil <nikhil342015@gmail.com>
2023-10-19 13:35:35 -0700

    Sign Up Stepper Template

--- lib/navbar.dart
+++ lib/navbar.dart
@@ -11,51 +11,39 @@ class _NavBarState extends State<NavBar> with TickerProviderStateMixin {
   Widget build(BuildContext context) {
     var brightness = MediaQuery.of(context).platformBrightness;
     bool darkModeOn = brightness == Brightness.dark;
-
-    double view_width = MediaQuery.of(context).size.width;
-    double tab_size = view_width * 0.2;
     return AppBar(
-      // backgroundColor: Colors.deepPurple,
-      // leading is the image, displayed before the title and tabbar
-      leadingWidth: 150,
-      // not entirely sure why 80 is the right number to center the image, but here we are
-      toolbarHeight: 80,
-
-      leading: Align(
-          alignment: Alignment.centerLeft,
-          child: Padding(
-              padding: const EdgeInsets.all(8),
-              // switches image based on dark mode
-              child: Image.asset((darkModeOn)
-                  ? "lib/images/logo_dark.png"
-                  : "lib/images/logo_light.png"))),
-      // title: const Text('Uni-Market'),
-      flexibleSpace: Align(
-          alignment: Alignment.topRight,
-          child: TabBar(
-            isScrollable: true,
-            controller: TabController(length: 3, vsync: this),
-            tabs: <Widget>[
-              SizedBox(
-                  width: tab_size,
-                  child: const Tab(
-                    text: 'Home',
-                    icon: Icon(Icons.home),
-                  )),
-              SizedBox(
-                  width: tab_size,
-                  child: const Tab(
-                    text: 'Search',
-                    icon: Icon(Icons.search),
-                  )),
-              SizedBox(
-                  width: tab_size,
-                  child: const Tab(
-                    text: 'Login',
-                    icon: Icon(Icons.login),
-                  )),
-            ],
-          )),
+      actions: <Widget>[
+        TextButton(
+          style: TextButton.styleFrom(
+            foregroundColor: Colors.white,
+            padding: const EdgeInsets.all(16.0),
+            textStyle: const TextStyle(fontSize: 20),
+          ),
+          onPressed: () {},
+          child: const Text('Sign Up'),
+        ),
+        const SizedBox(height: 20),
+        ClipRRect(
+          borderRadius: BorderRadius.circular(4),
+          child: Container(
+            color: Colors.white,
+            child: TextButton(
+              style: TextButton.styleFrom(
+                foregroundColor: const Color(0xFF041E42),
+                padding: const EdgeInsets.all(16.0),
+                textStyle: const TextStyle(fontSize: 20),
+              ),
+              onPressed: () {},
+              child: const Text('Sign In'),
+            ),
+          ),
+        ),
+      ],
+      backgroundColor: Theme.of(context).scaffoldBackgroundColor,
+      title: Image.asset(
+          (darkModeOn) ? 'assets/logo_dark.png' : 'assets/logo_light.png',
+          fit: BoxFit.contain,
+          height: 48),
     );
   }
 }
