fbc0fec869af0e817a2445ee1e775ea662811adb
cabmeron <116330722+cabmeron@users.noreply.github.com>
2023-10-23 16:23:51 -0700

    Refactored Register Form, single class

--- lib/register.dart
+++ lib/register.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:uni_market/registerForm.dart';
+import 'package:uni_market/constants.dart';
 
 Step Register(index) {
   return Step(
@@ -12,21 +12,83 @@ Step Register(index) {
       state: index >= 0 ? StepState.complete : StepState.disabled);
 }
 
-// Screen and Forms for Web Registering (Could habe logic for using each register screen per device)
-class WebRegisterScreen extends StatelessWidget {
-  // Web Register Screen Constructor
-  // Check if key exists for widget others set key to superclass key
-  const WebRegisterScreen({
+// Register Form
+class RegisterForm extends StatelessWidget {
+  const RegisterForm({
     Key? key,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: <Widget>[
-        RegisterForm(),
-      ],
-    );
+    return const Form(
+        child: Column(children: [
+      NameContainer(),
+      EmailContainer(),
+      PasswordContainer(),
+    ]));
+  }
+}
+
+// Email Input Field
+class EmailContainer extends StatelessWidget {
+  const EmailContainer({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+        keyboardType: TextInputType.emailAddress,
+        textInputAction: TextInputAction.next,
+        cursorColor: Colors.white,
+        onSaved: (email) {},
+        decoration: const InputDecoration(
+            hintText: "Email",
+            prefixIcon: Padding(
+              padding: EdgeInsets.all(defaultPadding),
+              child: Icon(Icons.email_rounded),
+            )));
+  }
+}
+
+// Name Input Field
+class NameContainer extends StatelessWidget {
+  const NameContainer({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+        keyboardType: TextInputType.name,
+        textInputAction: TextInputAction.next,
+        cursorColor: Colors.white,
+        onSaved: (email) {},
+        decoration: const InputDecoration(
+            hintText: "Full Name",
+            prefixIcon: Padding(
+              padding: EdgeInsets.all(defaultPadding),
+              child: Icon(Icons.person_2_rounded),
+            )));
+  }
+}
+
+// Password Input Field
+class PasswordContainer extends StatelessWidget {
+  const PasswordContainer({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+        textInputAction: TextInputAction.done,
+        obscureText: true,
+        cursorColor: Colors.white,
+        decoration: const InputDecoration(
+            hintText: "Password",
+            prefixIcon: Padding(
+                padding: EdgeInsets.all(defaultPadding),
+                child: Icon(Icons.lock_clock_rounded))));
   }
 }
