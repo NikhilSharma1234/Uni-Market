25cbed53434ae62549e1e54d81e7d5f6d6b8ea23
cabmeron <116330722+cabmeron@users.noreply.github.com>
2023-10-23 15:53:18 -0700

    registerForm and constants w/ step1

--- lib/register.dart
+++ lib/register.dart
@@ -1,19 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:uni_market/registerForm.dart';
 
 Step Register(index) {
   return Step(
       title: const Text('Register'),
       content: Container(
         alignment: Alignment.centerLeft,
-        child: const Text(
-          'This will be the content for Registration. Full Name, Email, Password',
-          style: TextStyle(
-            color: Colors.white,
-            fontSize: 12,
-            fontWeight: FontWeight.w500,
-          ),
-        ),
+        child: const RegisterForm(),
       ),
       isActive: index >= 0,
       state: index >= 0 ? StepState.complete : StepState.disabled);
+}
+
+// Screen and Forms for Web Registering (Could habe logic for using each register screen per device)
+class WebRegisterScreen extends StatelessWidget {
+  // Web Register Screen Constructor
+  // Check if key exists for widget others set key to superclass key
+  const WebRegisterScreen({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        RegisterForm(),
+      ],
+    );
+  }
 }
