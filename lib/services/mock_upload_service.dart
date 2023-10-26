dde23904d3f053c00e61ae6c3e6bc89c64d1369d
ChowdhuryYeamin <70544913+ChowdhuryYeamin@users.noreply.github.com>
2023-10-26 02:37:48 -0700

    3rd stepper designed, implemented strategy pattern for backend integration in future.

---
+++ lib/services/mock_upload_service.dart
@@ -0,0 +1,12 @@
+import 'upload_service.dart';
+
+class MockUploadService extends UploadService {
+  @override
+  Future<String?> uploadFile(Object file, String fileName) async {
+    // Simulate a delay for the mock upload.
+    await Future.delayed(Duration(seconds: 1));
+
+    // Return the file name.
+    return fileName;
+  }
+}
