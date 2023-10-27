017dd193727c986bff5cfd305e823024478c3ce6
ChowdhuryYeamin <70544913+ChowdhuryYeamin@users.noreply.github.com>
2023-10-26 21:44:30 -0700

    Check comments for backend implementation

--- lib/services/upload_service.dart
+++ lib/services/upload_service.dart
@@ -3,3 +3,5 @@ export 'upload_service.dart';
 abstract class UploadService {
   Future<String?> uploadFile(Object file, String fileName); // Return the name of the file
 }
+//implement uploadFile method in firebase. Parameters can be (object file, string fileName) if you need it for backend. Returning the file name to be displayed at the frontend. But, name of the file can directly extracted at the frontend
+//Currently, mockupload instance is used. Change it with firebase instace or appropriate method.
