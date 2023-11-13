30d0b9a0bff64f53b01bb61e788b5d9d78c60283
cabmeron <116330722+cabmeron@users.noreply.github.com>
2023-11-13 10:55:28 -0800

    user input states captured and registered with db

--- lib/register.dart
+++ lib/register.dart
@@ -46,7 +46,6 @@ class RegisterForm extends StatelessWidget {
                   if (_formKey.currentState!.validate()) {
                     // If the form is valid, display a snackbar. In the real world,
                     // you'd often call a server or save the information in a database.
-
                     ScaffoldMessenger.of(context).showSnackBar(
                       const SnackBar(content: Text('Creating your account')),
                     );
@@ -59,7 +58,6 @@ class RegisterForm extends StatelessWidget {
               ),
             ])));
   }
-
   // Function to reate Firebase User with Email and Password (Pass in Register Form Controllers)
   Future<void> _createUser(
     BuildContext context,
@@ -87,6 +85,8 @@ class RegisterForm extends StatelessWidget {
     }
   }
 }
+
+
 
 // Email Input Field
 class EmailContainer extends StatefulWidget {
@@ -150,7 +150,7 @@ class NameContainer extends StatefulWidget {
     Key? key,
     required this.nameController,
   }) : super(key: key);
-
+  
   @override
   State<NameContainer> createState() => _NameContainerState();
 }
@@ -180,6 +180,7 @@ class _NameContainerState extends State<NameContainer> {
               icon: const Icon(Icons.info_outlined)),
         ));
   }
+
 }
 
 class PasswordContainer extends StatefulWidget {
@@ -188,7 +189,7 @@ class PasswordContainer extends StatefulWidget {
     Key? key,
     required this.passwordController,
   }) : super(key: key);
-
+  
   @override
   State<PasswordContainer> createState() => _MyPasswordContainerState();
 }
