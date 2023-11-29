a48c0c613e61f2a703dba7eef35714987c09203e
ChowdhuryYeamin <70544913+ChowdhuryYeamin@users.noreply.github.com>
2023-11-28 23:18:49 -0800

    Improved user interface of AboutYou

--- lib/aboutyou.dart
+++ lib/aboutyou.dart
@@ -35,7 +35,7 @@ class _AboutYouContentState extends State<AboutYouContent> {
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
         _buildSchoolDropdown(),
-        const SizedBox(height: 16),
+        const SizedBox(height: 32),
         _buildFileUpload(
             title: 'File 1',
             onUpload: (url) {
@@ -43,7 +43,7 @@ class _AboutYouContentState extends State<AboutYouContent> {
                 firstFileName = url;
               });
             }),
-        const SizedBox(height: 16),
+        const SizedBox(height: 32),
         _buildFileUpload(
             title: 'File 2',
             onUpload: (url) {
@@ -55,39 +55,56 @@ class _AboutYouContentState extends State<AboutYouContent> {
     );
   }
 
-  // Function for school dropdown
-  Widget _buildSchoolDropdown() {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        const Text(
-          'School*',
-          style: TextStyle(
-            color: Colors.white,
-            fontSize: 12,
-            fontWeight: FontWeight.w500,
-          ),
+Widget _buildSchoolDropdown() {
+  List<String> list = ['University of Nevada, Reno', 'Test School', "Test School 2",  "Test School 3"]; // Replace with list of schools from database
+  String? dropdownValue;
+
+  return Column(
+    crossAxisAlignment: CrossAxisAlignment.start,
+    children: [
+      const Text(
+        'School*',
+        style: TextStyle(
+          color: Colors.white,
+          fontSize: 20,
+          fontWeight: FontWeight.w500,
         ),
-        const SizedBox(height: 8),
-        DropdownButton<String>(
-          value: selectedSchool,
-          hint: const Text('Select your school'),
-          onChanged: (String? newValue) {
-            setState(() {
-              selectedSchool = newValue;
-            });
-          },
-          items: <String>['University of Nevada, Reno', 'Test School']
-              .map<DropdownMenuItem<String>>((String value) {
-            return DropdownMenuItem<String>(
-              value: value,
-              child: Text(value),
-            );
-          }).toList(),
+      ),
+      const SizedBox(height: 12),
+      DropdownButtonFormField<String>(
+        value: dropdownValue, // The initial value of the dropdown
+        decoration: const InputDecoration(
+          // The box decoration of the dropdown
+          border: OutlineInputBorder(),
+          filled: true,
+          fillColor: Color.fromARGB(71, 11, 26, 103),
+          hintText: "Select your school", // Added hint text
+          hintStyle: TextStyle(fontSize: 16, color: Colors.white),// Style for the hint text
         ),
-      ],
-    );
-  }
+        items: list.map<DropdownMenuItem<String>>((String value) {
+          return DropdownMenuItem<String>(
+            value: value,
+            child: Text(
+              value,
+              style: const TextStyle(fontSize: 16, color: Colors.white), // Set font size for dropdown items
+            ),
+          );
+        }).toList(),
+        onChanged: (String? newValue) {
+          // This is called when the user selects an item.
+          setState(() {
+            dropdownValue = newValue!;
+          });
+        },
+        style: const TextStyle(fontSize: 16), // Set font size for the selected item displayed in the dropdown
+      ),
+    ],
+  );
+}
+
+
+
+
 
   // Function for file uploads
   Widget _buildFileUpload({
@@ -101,12 +118,12 @@ class _AboutYouContentState extends State<AboutYouContent> {
         title,
         style: const TextStyle(
           color: Colors.white,
-          fontSize: 12,
+          fontSize: 20,
           fontWeight: FontWeight.w500,
         ),
       ),
       Padding(   
-        padding: const EdgeInsets.only(top: 8.0),  
+        padding: const EdgeInsets.only(top: 16.0),  
         child: Row(
           children: [
             ElevatedButton(
