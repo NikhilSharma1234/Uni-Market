6b36bc5a710d479c8d3bdc4bf6347b48180f47e4
ChowdhuryYeamin <70544913+ChowdhuryYeamin@users.noreply.github.com>
2023-11-28 23:18:49 -0800

    Improved user interface of AboutYou

--- lib/aboutyou.dart
+++ lib/aboutyou.dart
@@ -57,123 +57,94 @@ class _AboutYouContentState extends State<AboutYouContent> {
     );
   }
 
-  Widget _buildSchoolDropdown() {
-    List<String> list = [
-      'University of Nevada, Reno',
-      'Test School',
-      "Test School 2",
-      "Test School 3"
-    ];
-    String? dropdownValue;
+Widget _buildSchoolDropdown() {
+  List<String> list = ['University of Nevada, Reno', 'Test School', "Test School 2",  "Test School 3"]; // Replace with list of schools from database
+  String? dropdownValue;
 
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        const Text(
-          'School*',
-          style: TextStyle(
-            color: Colors.white,
-            fontSize: 20,
-            fontWeight: FontWeight.w500,
-          ),
+  return Column(
+    crossAxisAlignment: CrossAxisAlignment.start,
+    children: [
+      const Text(
+        'School*',
+        style: TextStyle(
+          color: Colors.white,
+          fontSize: 20,
+          fontWeight: FontWeight.w500,
         ),
-        const SizedBox(height: 12),
-        DropdownButtonFormField<String>(
-          value: dropdownValue,
-          decoration: const InputDecoration(
-            border: OutlineInputBorder(),
-            filled: true,
-            fillColor: Color.fromARGB(71, 11, 26, 103),
-            hintText: "Select your school",
-            hintStyle: TextStyle(fontSize: 16, color: Colors.white),
-          ),
-          items: list.map<DropdownMenuItem<String>>((String value) {
-            return DropdownMenuItem<String>(
-              value: value,
-              child: Text(
-                value,
-                style: const TextStyle(fontSize: 16, color: Colors.white),
-              ),
-            );
-          }).toList(),
-          onChanged: (String? newValue) {
-            setState(() {
-              dropdownValue = newValue!;
-            });
-            if (newValue != null) {
-              uploadService.uploadSelectedSchool(newValue);
-            }
-          },
-          style: const TextStyle(fontSize: 16),
+      ),
+      const SizedBox(height: 12),
+      DropdownButtonFormField<String>(
+        value: dropdownValue, // The initial value of the dropdown
+        decoration: const InputDecoration(
+          // The box decoration of the dropdown
+          border: OutlineInputBorder(),
+          filled: true,
+          fillColor: Color.fromARGB(71, 11, 26, 103),
+          hintText: "Select your school", // Added hint text
+          hintStyle: TextStyle(fontSize: 16, color: Colors.white),// Style for the hint text
         ),
-      ],
-    );
-  }
+        items: list.map<DropdownMenuItem<String>>((String value) {
+          return DropdownMenuItem<String>(
+            value: value,
+            child: Text(
+              value,
+              style: const TextStyle(fontSize: 16, color: Colors.white), // Set font size for dropdown items
+            ),
+          );
+        }).toList(),
+        onChanged: (String? newValue) {
+          // This is called when the user selects an item.
+          setState(() {
+            dropdownValue = newValue!;
+          });
+        },
+        style: const TextStyle(fontSize: 16), // Set font size for the selected item displayed in the dropdown
+      ),
+    ],
+  );
+}
+
+
+
+
 
   Widget _buildFileUpload({
-    required String title,
-    required Function(String? url) onUpload,
-  }) {
-    ValueNotifier<bool> isUploading = ValueNotifier(false);
-
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Text(
-          title,
-          style: const TextStyle(
-            color: Colors.white,
-            fontSize: 20,
-            fontWeight: FontWeight.w500,
-          ),
+  required String title,
+  required Function(String? url) onUpload,
+}) {
+  return Column(
+    crossAxisAlignment: CrossAxisAlignment.start,
+    children: [
+      Text(
+        title,
+        style: const TextStyle(
+          color: Colors.white,
+          fontSize: 20,
+          fontWeight: FontWeight.w500,
         ),
-        Padding(
-          padding: const EdgeInsets.only(top: 16.0),
-          child: Row(
-            children: [
-              ElevatedButton(
-                onPressed: () async {
-                  FilePickerResult? result =
-                      await FilePicker.platform.pickFiles();
-                  isUploading.value = true;
-                  if (result != null) {
-                    String? uploadedFileName;
-                    if (kIsWeb) {
-                      Uint8List? fileBytes = result.files.single.bytes;
-                      String fileName = result.files.single.name;
-                      if (fileBytes != null) {
-                        uploadedFileName = await uploadService
-                            .uploadFileFromBytes(fileBytes, fileName);
-                      }
-                    } else {
-                      File file = File(result.files.single.path!);
-                      uploadedFileName = await uploadService.uploadFile(
-                          file, result.files.single.name);
-                    }
-                    onUpload(uploadedFileName);
-                    isUploading.value = false;
-                  } else {
-                    isUploading.value = false;
-                  }
-                },
-                child: Text('Upload $title'),
-              ),
-              const SizedBox(width: 10),
-              ValueListenableBuilder<bool>(
-                valueListenable: isUploading,
-                builder: (context, value, child) {
-                  if (value) {
-                    return CircularProgressIndicator();
-                  } else if ((title == 'File 1' && firstFileName != null) ||
-                      (title == 'File 2' && secondFileName != null)) {
-                    return Text(
-                        title == 'File 1' ? firstFileName! : secondFileName!);
-                  }
-                  return Container();
-                },
-              ),
-            ],
-          ),
+      ),
+      Padding(   
+        padding: const EdgeInsets.only(top: 16.0),  
+        child: Row(
+          children: [
+            ElevatedButton(
+              onPressed: () async {
+                FilePickerResult? result = await FilePicker.platform.pickFiles();
+                if (result != null)//backend is not
+                 {
+                  String? uploadedFileName = await uploadService.uploadFile(
+                      result.files.single, 
+                      result.files.single.name);
+                  onUpload(uploadedFileName);
+                }
+              },
+              child: Text('Upload $title'),
+            ),
+            const SizedBox(width: 10),
+            if ((title == 'File 1' && firstFileName != null) ||
+                (title == 'File 2' && secondFileName != null))
+              Text(title == 'File 1' ? firstFileName! : secondFileName!),
+          ],
         ),
       ],
     );
