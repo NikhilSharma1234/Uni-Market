6faf03cf4cd5f01a29076bcedd7f18bf9c9c3a65
ChowdhuryYeamin <70544913+ChowdhuryYeamin@users.noreply.github.com>
2024-01-17 00:54:53 -0800

    Backend intigrated for password reset

--- lib/pages/forgot_password.dart
+++ lib/pages/forgot_password.dart
@@ -1,56 +1,81 @@
 import 'package:flutter/material.dart';
 import 'package:uni_market/components/navbar.dart';
+import 'package:uni_market/services/email_manager.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
-class ForgotPasswordPage extends StatelessWidget {
-  ForgotPasswordPage({Key? key}) : super(key: key);
-
-  // Text Editing Controller for the email field
-  final TextEditingController emailController = TextEditingController();
-
-@override
-Widget build(BuildContext context) {
-  double screenWidth = MediaQuery.of(context).size.width;
-  double screenHeight = MediaQuery.of(context).size.height;
-
-  return Scaffold(
-    appBar: NavBar(),
-    body: Center(
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          SizedBox(
-            width: screenWidth < 500 ? screenWidth * 0.95 : screenWidth * 0.45,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                TextField(
-                  controller: emailController,
-                  decoration: InputDecoration(
-                    labelText: 'Email',
-                    border: OutlineInputBorder(),
-                  ),
-                  keyboardType: TextInputType.emailAddress,
-                ),
-                SizedBox(height: 20),
-                ElevatedButton(
-                  onPressed: () => _submitEmail(context),
-                  child: Text('Submit'),
-                ),
-              ],
-            ),
-          ),
-        ],
-      ),
-    ),
-  );
+class ForgotPasswordPage extends StatefulWidget {
+  @override
+  _ForgotPasswordPageState createState() => _ForgotPasswordPageState();
 }
 
+class _ForgotPasswordPageState extends State<ForgotPasswordPage> {
+  final TextEditingController emailController = TextEditingController();
+  final ValueNotifier<bool> submitEnabled = ValueNotifier<bool>(false);
+  late EmailManager emailManager;
+
+  @override
+  void initState() {
+    super.initState();
+    // Initialize emailManager here
+    emailManager = EmailManager(
+      FirebaseAuth.instance,
+      FirebaseFirestore.instance,
+    );
+  }
 
 
-  void _submitEmail(BuildContext context) {
-    // Implement the logic to handle password reset
+  @override
+  Widget build(BuildContext context) {
+    double screenWidth = MediaQuery.of(context).size.width;
+
+    return Scaffold(
+      appBar: NavBar(),
+      body: Center(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            SizedBox(
+              width: screenWidth < 500 ? screenWidth * 0.95 : screenWidth * 0.45,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  TextField(
+                    controller: emailController,
+                    decoration: InputDecoration(
+                      labelText: 'Email',
+                      border: OutlineInputBorder(),
+                    ),
+                    keyboardType: TextInputType.emailAddress,
+                  ),
+                  SizedBox(height: 20),
+                  ValueListenableBuilder<bool>(
+                    valueListenable: submitEnabled,
+                    builder: (context, isSubmitting, child) {
+                      return isSubmitting
+                        ? CircularProgressIndicator() // Show loading indicator when submitting
+                        : ElevatedButton(
+                            onPressed: () => _submitEmail(context),
+                            child: Text('Submit'),
+                          );
+                    },
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  void _submitEmail(BuildContext context) async {
+    submitEnabled.value = true;
     String email = emailController.text.trim();
-    // Use the email to send a password reset request
-    // Show a confirmation dialog/snackbar/message
+    try {
+      await emailManager.submitEmail(context, email);
+    } finally {
+      submitEnabled.value = false;
+    }
   }
 }
