46a754a66f5c5dab04bdc756c368fa0cfb5f69f1
Nikhil <nikhil342015@gmail.com>
2024-01-30 11:39:48 -0800

    Centering form

--- lib/pages/posting_page.dart
+++ lib/pages/posting_page.dart
@@ -12,19 +12,37 @@ class PostingPage extends StatefulWidget {
 class _PostingPageState extends State<PostingPage> {
   @override
   Widget build(BuildContext context) {
+    double screenWidth = MediaQuery.of(context).size.width;
+    //  More defined screen width for applicable form UI
+    widthScreen(double screenWidth) {
+      if (screenWidth < 600) {
+        return screenWidth * 0.95;
+      }
+      if (screenWidth < 800) {
+        return screenWidth * 0.75;
+      }
+      if (screenWidth < 1200) {
+        return screenWidth * 0.65;
+      }
+      return screenWidth * 0.45;
+    }
+
     return Scaffold(
-      appBar: const UserNavBar(), // Custom app bar here
-      body: Center(
-        child: ConstrainedBox(
-          constraints: const BoxConstraints(maxWidth: 500),
-          child: const SingleChildScrollView(
-            child: Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: PostForm(),
-            ),
-          ),
-        ),
-      ),
-    );
+        appBar: const UserNavBar(), // Custom app bar here
+        body: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  SizedBox(
+                    width: widthScreen(screenWidth),
+                    child: const PostForm(),
+                  ),
+                ],
+              )
+            ]));
   }
 }
