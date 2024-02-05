345e546da3ff8c7dd704bed5c7689eb8655513b9
jacobghunter <98200224+jacobghunter@users.noreply.github.com>
2024-02-05 13:23:18 -0800

    About us page basics

--- lib/pages/about_us.dart
+++ lib/pages/about_us.dart
@@ -32,11 +32,40 @@ class _AboutUsPageState extends State<AboutUsPage> {
     return Scaffold(
       // sets height of appbar
       appBar: NavBar(),
-      body: const Row(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      body: const Center(
+          child: Column(
         children: <Widget>[
+          Padding(
+            padding: EdgeInsets.only(bottom: 20),
+            child: Text(
+              "About Us",
+              style: TextStyle(fontSize: 60, fontWeight: FontWeight.bold),
+            ),
+          ),
+          Padding(
+              padding: EdgeInsets.only(bottom: 20),
+              child: Text("Project Title: Uni-Market",
+                  style: TextStyle(fontSize: 30))),
+          Padding(
+            padding: EdgeInsets.only(bottom: 10),
+            child: Text(
+                "Team 40: Jacob Hunter, Nikhil Sharma, Cameron McCoy, Yeamin Chowdhury",
+                style: TextStyle(fontSize: 20)),
+          ),
+          Padding(
+              padding: EdgeInsets.only(bottom: 10),
+              child: Text(
+                  "Instructors: Sara Davis, Vihn Le, Dr. Dave Feil-Seifer, Devrin Lee, and Sara Davis",
+                  style: TextStyle(fontSize: 20))),
+          Padding(
+              padding: EdgeInsets.only(bottom: 40),
+              child: Text("External Advisor: Dr. Eelke Folmer",
+                  style: TextStyle(fontSize: 20))),
+          Padding(
+              padding: EdgeInsets.only(bottom: 20), child: Text("description")),
+          Text("Project Resources: ")
         ],
-      ),
+      )),
     );
   }
 }
