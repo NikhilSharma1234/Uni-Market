d198cd23c8cf49d68f7261e166a28235a08259c2
NikhilSharma1234 <nikhil342015@gmail.com>
2024-02-10 15:30:08 -0800

    SignIn form fix

--- lib/pages/forgot_password.dart
+++ lib/pages/forgot_password.dart
@@ -5,6 +5,8 @@ import 'package:auto_size_text/auto_size_text.dart';
 import 'package:uni_market/components/register.dart';
 
 class ForgotPasswordPage extends StatefulWidget {
+  const ForgotPasswordPage({super.key});
+
   @override
   _ForgotPasswordPageState createState() => _ForgotPasswordPageState();
 }
