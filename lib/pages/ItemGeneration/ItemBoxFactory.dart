15c8e1a3b1296e2aa7c9798d525d5bdf042359cc
jacobghunter <98200224+jacobghunter@users.noreply.github.com>
2024-02-19 13:05:02 -0800

    Typesense searching DONE
    Need to implement filters and sorting, but the actual searching is working.

--- lib/pages/ItemGeneration/ItemBoxFactory.dart
+++ lib/pages/ItemGeneration/ItemBoxFactory.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/material.dart';
-import 'data.dart';
+import 'item.dart';
 import 'ItemBox.dart';
 import 'WebItemBox.dart';
 import 'MobileItemBox.dart';
 
 class ItemBoxFactory {
   late ItemBox item;
-  makeItemBox(Data data, BuildContext context) {
+  makeItemBox(Item data, BuildContext context) {
     bool isWeb = Theme.of(context).platform == TargetPlatform.windows ||
         Theme.of(context).platform == TargetPlatform.macOS;
     isWeb
