df0909bac83feb6885891d58aff73c80e54b3814
cabmeron <116330722+cabmeron@users.noreply.github.com>
2024-02-20 17:03:41 -0800

    Profile Page UI w/ backend, dark mode logic, no backend for profile pic

---
+++ lib/components/custom_switch.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+class CustomSwitch extends StatelessWidget {
+  final bool value;
+  final ValueChanged<bool> onChanged;
+
+  CustomSwitch({
+    Key? key,
+    required this.value,
+    required this.onChanged,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        onChanged(!value);
+      },
+      child: Container(
+        width: 45.0,
+        height: 28.0,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(24.0),
+          color: value ? Colors.blue.shade900 : Colors.grey,
+        ),
+        child: Padding(
+          padding: const EdgeInsets.only(
+            top: 2.0,
+            bottom: 2.0,
+            right: 2.0,
+            left: 2.0,
+          ),
+          child: Container(
+            alignment: value ? Alignment.centerRight : Alignment.centerLeft,
+            child: Container(
+              width: 20.0,
+              height: 20.0,
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                color: Colors.white,
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
