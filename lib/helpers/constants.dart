c90331d27508259cb642aeb0115bbeb6bb1f3be8
cabmeron <116330722+cabmeron@users.noreply.github.com>
2024-02-22 11:00:38 -0800

    profile pic field with initial signup image assignment

--- lib/helpers/constants.dart
+++ lib/helpers/constants.dart
@@ -1,2 +1,13 @@
 // For testing purposes not concrete
 const double defaultPadding = 16.0;
+
+const Map<int, String> stockProfilePics = {
+  0: 'astronaut.png',
+  1: 'bear.png',
+  2: 'beard.png',
+  3: 'cat.png',
+  4: 'dog.png',
+  5: 'gorilla.png',
+  6: 'llama.png',
+  7: 'rabbit.png',
+};
