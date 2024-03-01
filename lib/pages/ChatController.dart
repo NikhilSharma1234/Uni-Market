bb2081836de59ae56845dfa61aae3ebda0844054
ChowdhuryYeamin <70544913+ChowdhuryYeamin@users.noreply.github.com>
2024-02-29 17:00:51 -0800

    Added inbox with a list, chatpage is completed

---
+++ lib/pages/ChatController.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'ChatModel.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class ChatController {
+  final ChatModel _chatModel = ChatModel();
+  final TextEditingController messageController = TextEditingController();
+
+
+  Stream<QuerySnapshot> getMessageStream(String chatSessionId) {
+    return _chatModel.getMessagesStream(chatSessionId);
+  }
+
+  Future<void> sendMessage(String chatSessionId) async {
+    await _chatModel.sendMessage(chatSessionId, messageController.text.trim());
+    messageController.clear();
+  }
+  
+  // If you need to expose _chatModel, provide a public getter
+  ChatModel get chatModel => _chatModel;
+
+  
+}
