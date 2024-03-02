a41fa9a31ac8c02b926f227073342a6f6308795c
ChowdhuryYeamin <70544913+ChowdhuryYeamin@users.noreply.github.com>
2024-03-02 00:40:29 -0800

    Added divided between each chat in inbox

--- lib/pages/chat.dart
+++ lib/pages/chat.dart
@@ -49,75 +49,91 @@ class _ChatPageState extends State<ChatPage> {
     );
   }
 
-  Widget _buildMessageBubble(BuildContext context, QueryDocumentSnapshot message, bool isSentByMe) {
-  // Determine if we are in dark mode or light mode
-  bool isDarkMode = Theme.of(context).brightness == Brightness.dark;
+  Widget _buildMessageBubble(
+      BuildContext context, QueryDocumentSnapshot message, bool isSentByMe) {
+    // Determine if we are in dark mode or light mode
+    bool isDarkMode = Theme.of(context).brightness == Brightness.dark;
 
-  // Define bubble colors for light mode
-  Color senderBubbleColorLight = Colors.blueGrey.shade700;
-  Color receiverBubbleColorLight = Colors.blueGrey.shade700;
+    // Define bubble colors for light mode
+    Color senderBubbleColorLight = Colors.blueGrey.shade700;
+    Color receiverBubbleColorLight = Colors.blueGrey.shade700;
 
-  // Define bubble colors for dark mode
-  Color senderBubbleColorDark = Colors.white;
-  Color receiverBubbleColorDark = Colors.grey.shade200;
+    // Define bubble colors for dark mode
+    Color senderBubbleColorDark = Colors.white;
+    Color receiverBubbleColorDark = Colors.grey.shade200;
 
-  // Set bubble color and text color based on the theme and sender
-  Color bubbleColor;
-  Color textColor;
-  
-  if (isDarkMode) {
-    bubbleColor = isSentByMe ? senderBubbleColorDark : receiverBubbleColorDark;
-    textColor = Colors.black;
-  } else {
-    bubbleColor = isSentByMe ? senderBubbleColorLight : receiverBubbleColorLight;
-    textColor = Colors.white;
-  }
+    // Set bubble color and text color based on the theme and sender
+    Color bubbleColor;
+    Color textColor;
 
-  return Row(
-    mainAxisAlignment: isSentByMe ? MainAxisAlignment.end : MainAxisAlignment.start,
-    children: [
-      Container(
-        padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 10),
-        margin: const EdgeInsets.symmetric(horizontal: 8, vertical: 4),
-        decoration: BoxDecoration(
-          color: bubbleColor, // Use the determined bubble color
-          borderRadius: isSentByMe
-            ? const BorderRadius.only(
-                topLeft: Radius.circular(20),
-                bottomLeft: Radius.circular(20),
-                bottomRight: Radius.circular(20),
-              )
-            : const BorderRadius.only(
-                topRight: Radius.circular(20),
-                bottomLeft: Radius.circular(20),
-                bottomRight: Radius.circular(20),
-              ),
-        ),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
+    if (isDarkMode) {
+      bubbleColor =
+          isSentByMe ? senderBubbleColorDark : receiverBubbleColorDark;
+      textColor = Colors.black;
+    } else {
+      bubbleColor =
+          isSentByMe ? senderBubbleColorLight : receiverBubbleColorLight;
+      textColor = Colors.white;
+    }
+
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        // Calculate dynamic width here, e.g., a percentage of the parent's width
+        double bubbleMaxWidth =
+            constraints.maxWidth * 0.8; // Example: 80% of parent width
+
+        return Row(
+          mainAxisAlignment:
+              isSentByMe ? MainAxisAlignment.end : MainAxisAlignment.start,
           children: [
-            Text(
-              message['content'],
-              style: TextStyle(color: textColor),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(top: 4),
-              child: Text(
-                message['timestamp'].toDate().toString(),
-                style: TextStyle(
-                  color: textColor.withOpacity(0.7),
-                  fontSize: 10,
+            ConstrainedBox(
+              constraints: BoxConstraints(
+                maxWidth: bubbleMaxWidth, // Use dynamic width
+              ),
+              child: Container(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 16, vertical: 10),
+                margin: const EdgeInsets.symmetric(horizontal: 8, vertical: 4),
+                decoration: BoxDecoration(
+                  color: bubbleColor, // Use the determined bubble color
+                  borderRadius: isSentByMe
+                      ? const BorderRadius.only(
+                          topLeft: Radius.circular(20),
+                          bottomLeft: Radius.circular(20),
+                          bottomRight: Radius.circular(20),
+                        )
+                      : const BorderRadius.only(
+                          topRight: Radius.circular(20),
+                          bottomLeft: Radius.circular(20),
+                          bottomRight: Radius.circular(20),
+                        ),
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      message['content'],
+                      style: TextStyle(color: textColor),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(top: 4),
+                      child: Text(
+                        message['timestamp'].toDate().toString(),
+                        style: TextStyle(
+                          color: textColor.withOpacity(0.7),
+                          fontSize: 10,
+                        ),
+                      ),
+                    ),
+                  ],
                 ),
               ),
-            ),
+            )
           ],
-        ),
-      ),
-    ],
-  );
-}
-
-
+        );
+      },
+    );
+  }
 
   Widget _buildMessageComposer() {
     return Container(
