714cf3cbad3cc316284f175f8dd6673e92dd94ac
ChowdhuryYeamin <70544913+ChowdhuryYeamin@users.noreply.github.com>
2024-03-02 16:57:57 -0800

    inbox now gets all realtime updates

--- lib/pages/chat_summary.dart
+++ lib/pages/chat_summary.dart
@@ -1,15 +1,30 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
 class ChatSessionSummary {
   String sessionId;
   String buyerName;
-  String productName;  // Add this field
+  String productName;
   String lastMessage;
   DateTime lastMessageAt;
 
   ChatSessionSummary({
     required this.sessionId,
     required this.buyerName,
-    required this.productName,  // Update constructor
+    required this.productName,
     required this.lastMessage,
     required this.lastMessageAt,
   });
+
+  // Factory constructor to create a ChatSessionSummary from a Firestore DocumentSnapshot
+  factory ChatSessionSummary.fromDocument(DocumentSnapshot doc) {
+    Map<String, dynamic> data = doc.data() as Map<String, dynamic>;
+
+    return ChatSessionSummary(
+      sessionId: doc.id,
+      buyerName: data['buyerName'] ?? 'Unknown',
+      productName: data['productName'] ?? 'No Product Name',
+      lastMessage: data['lastMessage'] ?? 'No messages yet',
+      lastMessageAt: (data['lastMessageAt'] as Timestamp).toDate(),
+    );
+  }
 }
