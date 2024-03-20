1f669016a0fce550a579e5d88dad49cf42010d15
NikhilSharma1234 <nikhil342015@gmail.com>
2024-03-19 23:18:36 -0700

    Changes to cloud function

--- functions/main.py
+++ functions/main.py
@@ -2,8 +2,12 @@
 # To get started, simply uncomment the below code or create your own.
 # Deploy with `firebase deploy`
 
-from firebase_functions import https_fn
-from firebase_admin import initialize_app
+# Welcome to Cloud Functions for Firebase for Python!
+# To get started, simply uncomment the below code or create your own.
+# Deploy with `firebase deploy`
+
+from firebase_admin import firestore, initialize_app
+import google.cloud.firestore
 import typesense
 
 from firebase_functions.firestore_fn import (
@@ -16,42 +20,69 @@ from firebase_functions.firestore_fn import (
   DocumentSnapshot,
 )
 
-@on_document_created(document="items/{itemId}")
-def index_in_typesense(event: Event[DocumentSnapshot]) -> None:
-  client = typesense.Client({
+initialize_app()
+client = typesense.Client({
   'nodes': [{
-    'host': 'hawk-perfect-frog.ngrok-free.app', # For Typesense Cloud use xxx.a1.typesense.net
-    'port': '80',      # For Typesense Cloud use 443
-    'protocol': 'http'   # For Typesense Cloud use https
+    'host': 'hawk-perfect-frog.ngrok-free.app',
+    'port': '80',
+    'protocol': 'http'
   }],
   'api_key': 'eSMjP8YVxHdMKoT164TTKLMkXRS47FdDnPENNAA2Ob8RfEfr',
   'connection_timeout_seconds': 2,
   'connection_retries': 3
   })
 
+@on_document_created(document="items/{itemId}")
+def index_in_typesense(event: Event[DocumentSnapshot]) -> None:
+  item = event.data.to_dict()
+  
   client.collections['items'].documents.create({
-    'id': event.data['id'],
-    'buyerId': event.data['buyerId'],
-    'condition': event.data['condition'],
-    # dont think we need deletedAt since itll be gone from typesense anyway
-    'createdAt': event.data['createdAt'],
-    'dateUpdated': event.data['dateUpdated'],
-    'description': event.data['description'],
-    'images': event.data['images'],
-    'marketplaceId': event.data['marketplaceId'],
-    'name': event.data['name'],
-    'price': event.data['price'],
-    'schoolId': event.data['schoolId'],
-    'sellerId': event.data['sellerId'],
-    'tags': event.data['tags'],
-    'isFlagged': event.data['isFlagged']
-    # also dont think we need lastReviewed by
+    'id': event.params['itemId'],
+    'buyerId': item['buyerId'],
+    'condition': item['condition'],
+    'deletedAt': item['deletedAt'],
+    'createdAt': item['createdAt'],
+    'dateUpdated': item['dateUpdated'],
+    'description': item['description'],
+    'images': item['images'],
+    'marketplaceId': item['marketplaceId'],
+    'name': item['name'],
+    'price': item['price'],
+    'schoolId': item['schoolId'],
+    'sellerId': item['sellerId'],
+    'tags': item['tags'],
+    'isFlagged': item['isFlagged']
     })
-  print(f"Created document: {event.data}")
 
-# initialize_app()
-#
-#
-# @https_fn.on_request()
-# def on_request_example(req: https_fn.Request) -> https_fn.Response:
-#     return https_fn.Response("Hello world!")
+@on_document_created(document="typesense_sync/{backfillId}")
+def backfill_in_typesense(event: Event[DocumentSnapshot]) -> None:
+  backfill_dict = event.data.to_dict()
+  if event.params['backfillId'] == 'backfill' and backfill_dict['trigger'] == True:
+    firestore_client: google.cloud.firestore.Client = firestore.client()
+    docs = firestore_client.collection('items').stream()
+
+    for doc in docs:
+      item = doc.to_dict()
+      try:
+        # if doc exists
+        client.collections['items'].documents[str(doc.id)].retrieve()
+        break
+      except:
+        # if doc doesnt exist, add it
+        client.collections['items'].documents.create({
+        'id':doc.id,
+        'buyerId': item['buyerId'],
+        'condition': item['condition'],
+        'deletedAt': item['deletedAt'],
+        'createdAt': item['createdAt'],
+        'dateUpdated': item['dateUpdated'],
+        'description': item['description'],
+        'images': item['images'],
+        'marketplaceId': item['marketplaceId'],
+        'name': item['name'],
+        'price': item['price'],
+        'schoolId': item['schoolId'],
+        'sellerId': item['sellerId'],
+        'tags': item['tags'],
+        'isFlagged': item['isFlagged']
+        })
