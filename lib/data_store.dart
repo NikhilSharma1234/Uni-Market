72d05f96b3d2b67f7ce72f1cb957d21449eeee94
NikhilSharma1234 <nikhil342015@gmail.com>
2024-03-22 01:52:51 -0700

    Lazyload items

--- lib/data_store.dart
+++ lib/data_store.dart
@@ -1,25 +1,28 @@
 library data_store;
 
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
 import 'package:uni_market/data_models/current_user.dart';
 
 CurrentUser user = CurrentUser(
   assignable_profile_pic: null,
   createdAt: Timestamp.now(),
   darkMode: 0,
-  deletedAt : null,
-  email : "",
-  emailVerified : false,
-  marketplaceId : "",
-  name : "",
-  schoolId : "",
-  starting_profile_pic : "",
+  deletedAt: null,
+  email: "",
+  emailVerified: false,
+  marketplaceId: "",
+  name: "",
+  schoolId: "",
+  starting_profile_pic: "",
   starting_profile_pic_url: "",
-  updatedAt : Timestamp.now(),
-  verificationDocsUploaded : false,
-  verifiedUniStudent : false,
+  updatedAt: Timestamp.now(),
+  verificationDocsUploaded: false,
+  verifiedUniStudent: false,
   verifiedBy: null,
   verifiedAt: null,
   institutionFullName: '',
   schoolsInMarketplace: [],
 );
+
+List<Widget> itemBoxes = [];
