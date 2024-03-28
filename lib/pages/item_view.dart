780b7fda553a21674647f58c7ce89fa95a6acee2
jacobghunter <98200224+jacobghunter@users.noreply.github.com>
2024-03-28 16:21:47 -0700

    Routing to page for created post after creation

--- lib/pages/item_view.dart
+++ lib/pages/item_view.dart
@@ -3,6 +3,9 @@ import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 import 'package:uni_market/components/ItemGeneration/abstract_item_factory.dart';
 import 'package:uni_market/components/ItemGeneration/item.dart';
+import 'package:uni_market/helpers/functions.dart';
+import 'package:web_smooth_scroll/web_smooth_scroll.dart';
+import 'package:flutter/foundation.dart';
 
 class ItemView extends StatefulWidget {
   final String? sellerID;
@@ -21,16 +24,6 @@ class _ItemViewState extends State<ItemView> {
   late List<Widget> items = [];
   var db = FirebaseFirestore.instance;
   AbstractItemFactory factory = AbstractItemFactory();
-
-  Future<String> getURL(String imageURL) async {
-    String image;
-    try {
-      image = await FirebaseStorage.instance.ref(imageURL).getDownloadURL();
-    } catch (e) {
-      image = "Missing Image";
-    }
-    return image;
-  }
 
   getItems() async {
     List<Widget> foundItemsArr = [];
@@ -60,6 +53,16 @@ class _ItemViewState extends State<ItemView> {
     return foundItemsArr;
   }
 
+  late ScrollController _scrollController;
+
+  @override
+  void initState() {
+    // initialize scroll controllers
+    _scrollController = ScrollController();
+
+    super.initState();
+  }
+
   @override
   void didChangeDependencies() {
     getItems().then((value) => setState((() {
@@ -70,19 +73,71 @@ class _ItemViewState extends State<ItemView> {
 
   @override
   Widget build(BuildContext context) {
+    double screenWidth = MediaQuery.of(context).size.width;
+
     if (widget.sellerID == null) {
       items = [const Text("Issue getting user")];
     }
 
     Widget body;
 
-    body = SingleChildScrollView(child: Wrap(children: items));
+    if (kIsWeb) {
+      var itemSize = screenWidth / (screenWidth / 320).round();
+      for (final (index, item) in items.indexed) {
+        items[index] =
+            (SizedBox(width: itemSize, height: itemSize, child: item));
+      }
+      int rowSize = (screenWidth / 320).round();
+      int numRows = (items.length / rowSize).round();
+      if (numRows == 0) {
+        numRows = 1;
+      }
+
+      List<Widget> rows = [];
+      int currentItem = 0;
+      for (var row = 0; row < numRows; row++) {
+        int endIndex = currentItem + rowSize;
+        if (currentItem + rowSize > items.length) {
+          endIndex = items.length;
+        }
+        rows.add(Row(children: items.sublist(currentItem, endIndex)));
+        currentItem = endIndex;
+      }
+      body = WebSmoothScroll(
+        controller: _scrollController,
+        scrollOffset: 100,
+        animationDuration: 400,
+        child: SingleChildScrollView(
+          physics: const NeverScrollableScrollPhysics(),
+          controller: _scrollController,
+          child: Column(children: rows),
+        ),
+      );
+    } else {
+      body = GridView.count(
+        physics: const BouncingScrollPhysics(
+            parent: AlwaysScrollableScrollPhysics()),
+        crossAxisCount: (screenWidth / 320).round(),
+        childAspectRatio: 2 / 2,
+        children: items,
+      );
+    }
 
     if (items.isEmpty) {
       body = const Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[Text('No Items Listed or still Loading items')],
+          children: <Widget>[
+            SizedBox(
+              width: 60,
+              height: 60,
+              child: CircularProgressIndicator(),
+            ),
+            Padding(
+              padding: EdgeInsets.only(top: 16),
+              child: Text('Awaiting result...'),
+            ),
+          ],
         ),
       );
     }
