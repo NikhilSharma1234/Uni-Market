9910932b4d12bacff36785ddb4c19850f360616c
jacobghunter <98200224+jacobghunter@users.noreply.github.com>
2024-04-01 20:38:42 -0700

    Fixed home page and rebased
    Also added seeing your own post at the top of search results right after making it.

--- lib/components/ItemGeneration/item_box.dart
+++ lib/components/ItemGeneration/item_box.dart
@@ -59,105 +59,80 @@ class _ItemBoxState extends State<ItemBox> {
         fit: BoxFit.fill,
       );
     }
-    itemsWidth(double screenWidth) {
-      if (screenWidth < 500) {
-        return (screenWidth - (screenWidth * 0.06));
-      }
-      if (screenWidth < 650) {
-        return (screenWidth - (screenWidth * 0.06)) / 2;
-      }
-      if (screenWidth < 1000) {
-        return (screenWidth - (screenWidth * 0.06)) / 3;
-      }
-      if (screenWidth < 1300) {
-        return (screenWidth - (screenWidth * 0.06)) / 4;
-      }
-      if (screenWidth < 1600) {
-        return (screenWidth - (screenWidth * 0.06)) / 5;
-      }
-      if (screenWidth < 2000) {
-        return (screenWidth - (screenWidth * 0.06)) / 6;
-      }
-      if (screenWidth < 2400) {
-        return (screenWidth - (screenWidth * 0.06)) / 7;
-      }
-      return (screenWidth - (screenWidth * 0.06)) / 4;
-    }
 
-    double itemWidth = itemsWidth(MediaQuery.of(context).size.width);
-    double itemHeight = itemWidth * 1.15;
     Color conditionColor = conditionBackground[item.condition] ?? Colors.black;
     return Padding(
         padding: const EdgeInsets.all(4),
         child: InkWell(
             // for future use to link each item to a unique page based on its id
             onTap: () {
-              Navigator.of(context).push(
-                MaterialPageRoute(
-                  builder: (context) => ItemPage(data: item),
-                ),
-              );
-            },
-            child: Container(
-                width: itemWidth,
-                clipBehavior: Clip.antiAlias,
-                decoration: BoxDecoration(
-                    color: Theme.of(context).brightness == Brightness.light
-                        ? Colors.white
-                        : Colors.black,
-                    borderRadius: BorderRadius.circular(8)),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Container(
-                        decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(8)),
-                        height: itemHeight * 0.75,
-                        width: itemWidth,
-                        child: image),
-                    Padding(
-                      padding: const EdgeInsets.only(left: 8, right: 8),
-                      child: Text(item.name,
-                          style: const TextStyle(
-                              fontSize: 20, overflow: TextOverflow.ellipsis)),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.only(left: 8, right: 8),
-                      child: Text(item.description,
-                          style: const TextStyle(
-                              color: Colors.grey,
-                              fontSize: 20,
-                              overflow: TextOverflow.ellipsis)),
-                    ),
-                    Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Padding(
-                            padding: const EdgeInsets.only(left: 8, right: 8),
-                            child: Text('\$${item.price.toDouble().toString()}',
-                                style: const TextStyle(
-                                    fontSize: 20,
-                                    overflow: TextOverflow.ellipsis)),
-                          ),
-                          Padding(
-                              padding: const EdgeInsets.only(
-                                  left: 8, right: 8, bottom: 4),
-                              child: Container(
-                                decoration: BoxDecoration(
-                                    borderRadius: const BorderRadius.all(
-                                        Radius.circular(5)),
-                                    color: conditionColor),
-                                child: Padding(
-                                  padding:
-                                      const EdgeInsets.symmetric(horizontal: 3),
-                                  child: Text(
-                                      '${item.condition[0]}${item.condition.substring(1).toLowerCase()}',
-                                      style: const TextStyle(fontSize: 20)),
-                                ),
-                              ))
-                        ]),
-                  ],
-                ))));
+          Navigator.of(context).push(
+            MaterialPageRoute(
+              builder: (context) => ItemPage(data: item),
+            ),
+          );
+        }, child: LayoutBuilder(
+                builder: (BuildContext context, BoxConstraints constraints) {
+          return Container(
+              clipBehavior: Clip.antiAlias,
+              decoration: BoxDecoration(
+                  color: Theme.of(context).brightness == Brightness.light
+                      ? Colors.white
+                      : Colors.black,
+                  borderRadius: BorderRadius.circular(8)),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Container(
+                      decoration:
+                          BoxDecoration(borderRadius: BorderRadius.circular(8)),
+                      height: constraints.maxHeight * 0.74,
+                      width: constraints.maxWidth,
+                      child: image),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 8, right: 8),
+                    child: Text(item.name,
+                        style: const TextStyle(
+                            fontSize: 20, overflow: TextOverflow.ellipsis)),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 8, right: 8),
+                    child: Text(item.description,
+                        style: const TextStyle(
+                            color: Colors.grey,
+                            fontSize: 20,
+                            overflow: TextOverflow.ellipsis)),
+                  ),
+                  Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.only(left: 8, right: 8),
+                          child: Text('\$${item.price.toDouble().toString()}',
+                              style: const TextStyle(
+                                  fontSize: 20,
+                                  overflow: TextOverflow.ellipsis)),
+                        ),
+                        Padding(
+                            padding: const EdgeInsets.only(
+                                left: 8, right: 8, bottom: 7),
+                            child: Container(
+                              decoration: BoxDecoration(
+                                  borderRadius: const BorderRadius.all(
+                                      Radius.circular(7)),
+                                  color: conditionColor),
+                              child: Padding(
+                                padding:
+                                    const EdgeInsets.symmetric(horizontal: 3),
+                                child: Text(
+                                    '${item.condition[0]}${item.condition.substring(1).toLowerCase()}',
+                                    style: const TextStyle(fontSize: 20)),
+                              ),
+                            ))
+                      ]),
+                ],
+              ));
+        })));
   }
 }
