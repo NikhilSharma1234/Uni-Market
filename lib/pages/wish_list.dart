3b18ec68e346200b73ef2bba3d81d5af5c88a0b7
jacobghunter <98200224+jacobghunter@users.noreply.github.com>
2024-04-01 20:58:39 -0700

    Wishlist and items youve listed pages fixed

--- lib/pages/wish_list.dart
+++ lib/pages/wish_list.dart
@@ -1,12 +1,9 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_storage/firebase_storage.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
 import 'package:uni_market/components/ItemGeneration/abstract_item_factory.dart';
 import 'package:uni_market/components/ItemGeneration/item.dart';
-import 'package:uni_market/helpers/theme_provider.dart';
-import 'package:uni_market/pages/item_page.dart';
+import 'package:uni_market/helpers/functions.dart';
 import 'package:uni_market/data_store.dart' as data_store;
 
 class WishList extends StatefulWidget {
@@ -21,201 +18,99 @@ class WishList extends StatefulWidget {
 }
 
 class _WishListState extends State<WishList> {
-  late Stream<List<Widget>> itemStream;
+  late List<Widget> items = [
+    const Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Padding(padding: EdgeInsets.only(top: 16)),
+          SizedBox(
+            width: 60,
+            height: 60,
+            child: CircularProgressIndicator(),
+          ),
+          Padding(
+            padding: EdgeInsets.symmetric(vertical: 16),
+            child: Text('Awaiting result...'),
+          ),
+        ],
+      ),
+    )
+  ];
+  var db = FirebaseFirestore.instance;
   AbstractItemFactory factory = AbstractItemFactory();
-  itemsWidth(double screenWidth) {
-    if (screenWidth < 500) {
-      return (screenWidth - (screenWidth * 0.06));
-    }
-    if (screenWidth < 650) {
-      return (screenWidth - (screenWidth * 0.06)) / 2;
-    }
-    if (screenWidth < 1000) {
-      return (screenWidth - (screenWidth * 0.06)) / 3;
-    }
-    if (screenWidth < 1300) {
-      return (screenWidth - (screenWidth * 0.06)) / 4;
-    }
-    if (screenWidth < 1600) {
-      return (screenWidth - (screenWidth * 0.06)) / 5;
-    }
-    if (screenWidth < 2000) {
-      return (screenWidth - (screenWidth * 0.06)) / 6;
-    }
-    if (screenWidth < 2400) {
-      return (screenWidth - (screenWidth * 0.06)) / 7;
-    }
-    return (screenWidth - (screenWidth * 0.06)) / 4;
-  }
 
-  Map<String, Color> conditionBackground = {
-    "NEW": Colors.green,
-    "USED": Colors.orange,
-    "WORN": Colors.red
-  };
+  getItems() async {
+    List<Widget> foundItemsArr = [];
 
-  Future<Widget> generateItemWidget(itemId, darkModeOn) async {
-    var itemFromFirebase =
-        await FirebaseFirestore.instance.collection("items").doc(itemId).get();
-    if (itemFromFirebase.data() != null) {
-      var item = itemFromFirebase.data();
-      item?['id'] = itemFromFirebase.id; // adding id to data
-      if (item?['images'].length == 0) {
-        item?['images'].add(await FirebaseStorage.instance
+    List wishListedItems = [];
+
+    for (var item in data_store.user.wishlist) {
+      wishListedItems.add(await db.collection("items").doc(item).get());
+    }
+
+    for (var docSnap in wishListedItems) {
+      var item = docSnap.data();
+      item['id'] = docSnap.id; // adding id to data
+
+      if (item['images'].length == 0) {
+        item['images'].add(await FirebaseStorage.instance
             .ref("images/missing_image.jpg")
             .getDownloadURL());
       } else {
-        for (int i = 0; i < item?['images'].length; i++) {
-          item?['images'][i] = await getURL(item['images'][i]);
+        for (int i = 0; i < item['images'].length; i++) {
+          item['images'][i] = await getURL(item['images'][i]);
         }
       }
-      try {
-        // ignore: use_build_context_synchronously
-        double itemWidth = itemsWidth(MediaQuery.of(context).size.width);
-        double itemHeight = itemWidth * 1.15;
-
-        return InkWell(
-          onTap: () {
-            Navigator.of(context).push(
-              MaterialPageRoute(
-                builder: (context) => ItemPage(data: Item.fromFirebase(item)),
-              ),
-            );
-          },
-          child: Padding(
-            padding: const EdgeInsets.all(4.0),
-            child: Container(
-                width: itemWidth,
-                clipBehavior: Clip.antiAlias,
-                decoration: BoxDecoration(
-                    color: darkModeOn ? Colors.black : Colors.white,
-                    borderRadius:
-                        BorderRadius.circular(8) // Adjust the radius as needed
-                    ),
-                child: Column(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      SizedBox(
-                        height: itemHeight * 0.75,
-                        width: itemWidth,
-                        child: Image.network(
-                          item?['images'][0],
-                          fit: BoxFit.fill,
-                        ),
-                      ),
-                      Padding(
-                        padding:
-                            const EdgeInsets.only(top: 4, left: 8, right: 8),
-                        child: Text(item?['name'], maxLines: 1),
-                      ),
-                      Padding(
-                          padding:
-                              const EdgeInsets.only(left: 8, right: 8, top: 2),
-                          child: Text(
-                            item?['description'],
-                            maxLines: 1,
-                            style: const TextStyle(color: Colors.grey),
-                          )),
-                      Padding(
-                        padding: const EdgeInsets.only(
-                            top: 4, left: 8, right: 8, bottom: 4),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Text('\$${item!['price'].toDouble().toString()}',
-                                maxLines: 1),
-                            Container(
-                                decoration: BoxDecoration(
-                                    color: conditionBackground[
-                                            item['condition']] ??
-                                        Colors.transparent,
-                                    borderRadius: BorderRadius.circular(3)),
-                                child: Padding(
-                                  padding: const EdgeInsets.all(4),
-                                  child: Text(item['condition']),
-                                ))
-                          ],
-                        ),
-                      )
-                    ])),
-          ),
-        );
-      } catch (e) {
-        if (kDebugMode) {
-          print(e);
-        }
+      if (context.mounted) {
+        foundItemsArr
+            .add(factory.buildItemBox(Item.fromFirebase(item), context));
       }
     }
-    return const SizedBox(
-      width: 30,
-      child: Text('Abasad'),
-    );
+    return foundItemsArr;
   }
 
-  Future<String> getURL(String imageURL) async {
-    String image;
-    try {
-      image = await FirebaseStorage.instance.ref(imageURL).getDownloadURL();
-    } catch (e) {
-      image = "Missing Image";
-    }
-    return image;
-  }
-
-  Future<List<Widget>> generateItems(itemIds, darkModeOn) async {
-    List<Widget> itemsList = [];
-    for (var id in itemIds) {
-      var item = await generateItemWidget(id, darkModeOn);
-      itemsList.add(item);
-    }
-    return itemsList;
+  @override
+  @override
+  void didChangeDependencies() {
+    getItems().then((value) => setState((() {
+          if (items.isEmpty) {
+            items = [const Text("No Items Wishlisted")];
+          } else {
+            items = value;
+          }
+        })));
+    super.didChangeDependencies();
   }
 
   @override
   Widget build(BuildContext context) {
-    bool darkModeOn =
-        // ignore: use_build_context_synchronously
-        Provider.of<ThemeProvider>(context, listen: true).themeMode ==
-            ThemeMode.dark;
+    double screenWidth = MediaQuery.of(context).size.width;
+
+    Widget body;
+
+    body = GridView.count(
+        physics: const BouncingScrollPhysics(
+            parent: AlwaysScrollableScrollPhysics()),
+        crossAxisCount: (screenWidth / 320).round(),
+        childAspectRatio: 20 / 23,
+        children: items);
+
+    if (items.isEmpty) {
+      body = const Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text('No Items Wishlisted or still Loading items')
+          ],
+        ),
+      );
+    }
+
     return Scaffold(
         appBar: AppBar(
-          title: const Text("Wishlist Items"),
+          title: const Text("Your Items"),
         ),
-        body: StreamBuilder(
-          stream: FirebaseFirestore.instance
-              .collection('users')
-              .doc(data_store.user.email)
-              .snapshots(),
-          builder: (context, streamSnapshot) {
-            if (streamSnapshot.hasData) {
-              return FutureBuilder(
-                  future: generateItems(
-                      streamSnapshot.data!['wishlist'], darkModeOn),
-                  builder: (context, futureSnapshot) {
-                    if (futureSnapshot.hasData) {
-                      var items = futureSnapshot.data ?? [const SizedBox()];
-                      return SingleChildScrollView(
-                        child: Wrap(children: items),
-                      );
-                    }
-                    return const Row(
-                      children: [
-                        Expanded(
-                          child: Center(child: CircularProgressIndicator()),
-                        ),
-                      ],
-                    );
-                  });
-            }
-            return const Row(
-              children: [
-                Expanded(
-                  child: Center(child: CircularProgressIndicator()),
-                ),
-              ],
-            );
-          },
-        ));
+        body: body);
   }
 }
